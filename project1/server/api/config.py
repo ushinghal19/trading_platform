6e86f9529352cc0f81d5793a6aa163da6d44335b
Utsav Shinghal <56486015+ushinghal19@users.noreply.github.com>
2024-03-12 02:36:45 +0800

    Update config.py

--- project1/server/api/config.py
+++ project1/server/api/config.py
@@ -1,6 +1,6 @@
-ALPHA_VANTAGE_API_KEY = 'EYQ9GU2BZFMXJJ33'
+ALPHA_VANTAGE_API_KEY = '{Insert your API Key Here}'
 ALPHA_VANTAGE_BASE_URL = 'https://www.alphavantage.co/query'
-FINNHUB_API_KEY = 'cnmipt1r01qtghmdpd70cnmipt1r01qtghmdpd7g'
+FINNHUB_API_KEY = '{Insert your API Key Here}'
 FINNHUB_BASE_URL = 'https://finnhub.io/api/v1'
 ALPHAVANTAGE_CALLS = 5
 ALPHVANTAGE_SECONDS = 60
